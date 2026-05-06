a06abb89f90e62d1d8e46bf4a681fafd0ad26927
Nelson Izquierdo <izquierdonelson@gmail.com>
2026-05-05 21:24:33 -0400

    chore(wallet): use Request in api

--- src/app/api/wallet/apple/v1/log/route.ts
+++ src/app/api/wallet/apple/v1/log/route.ts
@@ -1,6 +1,6 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {
+export async function POST(request: Request) {
   const body = await request.json();
   console.log(body);
   return NextResponse.json(null, { status: 200 });
